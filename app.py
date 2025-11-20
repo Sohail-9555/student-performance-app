0cfa35ca3c9746f9c5ee1ea9dfdcdd36f5c01ccd
Sohail Jamal <jamalsohail464@gmail.com>
2025-11-21 02:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sklearn.externals import joblib
+import joblib
 import numpy as np
 import pandas as pd
 
@@ -32,5 +32,6 @@ if st.button("Predict Score"):
     prediction = model.predict(input_data)[0]
     st.success(f"Predicted Total Score: **{prediction:.2f}**")
     
+
 
 
